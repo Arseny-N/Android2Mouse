2202d04fc231121071a78dcdc2b0f62348cb52b6
Aresni Nerinovsky <arseny-n@yandex.ru>
2014-05-08 14:47:31 +0400

    moreinfo

--- src/bin/hidp_clientd/log.h
+++ src/bin/hidp_clientd/log.h
@@ -36,7 +36,7 @@ struct log {
 #define dbg(f, a...) _dbg(f "\n", ##a)
 #elif defined STDOUT_LOG
 
-#define _error(f, a...) fprintf (stderr," ERR: %16s:%s (%.2d): " f ,__func__,strerror(errno),errno, ##a))
+#define _error(f, a...) fprintf(stderr," ERR: %16s:%s (%.2d): " f ,__func__,strerror(errno),errno, ##a))
 #define _info(f, a...)  fprintf(stderr,"INFO: %16s:    " f ,__func__, ##a)
 #define _dbg(f, a...)   fprintf(stderr," DBG: %16s:    " f, __func__, ##a)
 
